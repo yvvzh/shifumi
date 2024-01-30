ef248a2aa3cf2b933118e4c6c651730197c03257
yvvzh <135761301+yvvzh@users.noreply.github.com>
2024-01-30 11:13:02 +0100

    design done

---
+++ shifumi.js
@@ -0,0 +1,21 @@
+const waterBtn = document.getElementById("water-btn");
+const fireBtn = document.getElementById("fire-btn");
+const plantBtn = document.getElementById("plant-btn");
+const dialDisplay = document.getElementById("display");
+const playerScoreDisplay = document.getElementById("player-score");
+const computerScoreDisplay = document.getElementById("computer-score");
+
+let welcomeDial =
+    "Welcome to Elementals. This is a 'shifumi' style game. You have to pick an element and hope that the computer picks the opposite weak element. First to 3 wins. Good luck!";
+let roundDial = "Computer chose '...'. '...' won this round!";
+let gameDial = "... won this game!";
+let playerScore = 0;
+let computerScore = 0;
+
+function computerPick() {}
+
+function hadjime() {}
+
+function displayText() {}
+
+function displayScores() {}
